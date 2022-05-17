02515be23b6fdae474c417dc3df63ad4023bd07c
IT21285042 <99561023+IT21285042@users.noreply.github.com>
2022-05-17 18:08:41 +0000

    I did it

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,36 @@
+[11:31 PM] Rasnakawewa C.V.B it21285738
+#include<iostream>
 #include "Box.h"
-
-// Implement setters and getters
-
-// Implemenet the calcVolume() unction
-int Box::calcVolume() {
+using namespace std;// Implement setters and getters
+void Box :: setMeasurements(int l, int w, int h)
+{
+length = l;
+width = w;
+height = h;
+}int Box :: getLength()
+{
+return length;
+}int Box :: getWidth()
+{
+return width;
+}int Box :: getHeight()
+{
+return height;
 }
+// Implemenet the calcVolume() function
+int Box::calcVolume()
+{
+return length * width * height;
+}
+
+[11:31 PM] Rasnakawewa C.V.B it21285738
+class Student
+{
+private:
+int studentId;
+char name[20];
+public:
+void assignDetails(int sId, char sName[]);
+void display();
+};
+
